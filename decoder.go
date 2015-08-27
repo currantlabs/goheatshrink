6f07e6564648cf9c04ab21b44f63ad7a6ec5cf80
Hasty Granbery <hasty@granbery.org>
2015-08-26 23:15:24 -0700

    More idiomatic, and fewer allocations (4x faster!)

--- decoder.go
+++ decoder.go
@@ -1,7 +1,6 @@
 package goheatshrink
 
 import (
-	"bytes"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -9,98 +8,87 @@ import (
 	"log"
 )
 
-type Decoder interface {
-	Decode(in []byte) ([]byte, error)
-}
+type Reader struct {
+	reader io.Reader
+	window    uint8
+	lookahead uint8
 
-type decoder struct {
+	inputBuffer []byte
 	inputSize   int
 	inputIndex  int
-	outputCount int
-	outputIndex int
+
 	headIndex   int
 	state       decodeState
 	current     byte
 	bitIndex    byte
 
 	buffer       []byte
-	window       uint8
 	windowBuffer []byte
-	lookahead    uint8
 	bufferSize   int
-}
 
-type outputInfo struct {
-	buf        []byte
-	bufSize    int
+	outputBuffer []byte
+	outputCount int
 	outputSize int
+	outputIndex int
+	outputBackRefIndex int
 }
 
-const NOBITS = ^uint16(0)
+const nobits = ^uint16(0)
 
-func NewDecoder(window uint8, lookahead uint8) Decoder {
-	return &decoder{
+var errOutputBufferFull = errors.New("output buffer full")
+
+func NewReader(reader io.Reader, window uint8, lookahead uint8) *Reader {
+	return &Reader{
+		reader: reader,
+		window:    window,
+		lookahead: lookahead,
 		state:        decodeStateTagBit,
-		window:       window,
-		lookahead:    lookahead,
 		windowBuffer: make([]byte, 1<<window),
+		inputBuffer: make([]byte, 1 << window),
 	}
 }
 
-func (d *decoder) Decode(in []byte) ([]byte, error) {
-	var out []byte
-	ow := bytes.NewBuffer(out)
-	chunkSize := 1 << d.window
-	s := len(in)
-	for {
-		if s == 0 {
-			d.finish()
-			break
-		}
-		var s1 int
-		if s > chunkSize {
-			s1 = chunkSize
-			s -= chunkSize
-		} else {
-			s1 = s
-			s = 0
-		}
-		l("Chunking %v bytes...\n", len(in))
-		d.decodeRead(in[:s1], ow)
-		in = in[s1:]
+func (r *Reader) Read(out []byte) (int, error) {
+	var in []byte
+	if r.inputSize > 0 {
+		// Still have leftover bytes from last read
+		in = r.inputBuffer[r.inputSize:]
+	} else {
+		in = r.inputBuffer
 	}
-	return ow.Bytes(), nil
+	incount, err := r.reader.Read(in)
+	r.inputSize += incount
+	if r.inputSize > 0 {
+		return r.decodeRead(r.inputBuffer[:r.inputSize], out)
+	} else if err == io.EOF && r.finish() {
+		return 0, io.EOF
+	}
+	return 0, errors.New("Ran out of input before finishing")
 }
 
-func (d *decoder) decodeRead(in []byte, out io.Writer) error {
-	outBuf := make([]byte, 256)
-	total := 0
-	for total < len(in) {
-		inputSize := d.sink(in)
-		total += inputSize
-		for {
-			err, outputSize, outputBufferFull := d.poll(outBuf)
-			out.Write(outBuf[:outputSize])
-			l("Poll: %v %v %v %v\n", err, outputSize, outputBufferFull, total)
-			if !outputBufferFull {
-				break
-			}
-			l("Output buffer full, continue polling...")
-		}
-		if d.finish() {
-			return nil
-		}
-		break
-	}
-	return errors.New("Ran out of input before finishing")
-}
-
-func (d *decoder) sink(in []byte) int {
+func (r *Reader) decodeRead(in []byte, out []byte) (int, error) {
+	totalout := 0
+	r.buffer = in
+	r.inputSize = len(in)
+	r.outputBuffer = out
+	r.outputSize = len(out)
+	r.outputIndex = 0
 	l("-- sinking %d bytes\n", len(in))
 	l("buffer: %v", hex.EncodeToString(in))
-	d.buffer = in
-	d.inputSize += len(in)
-	return d.inputSize
+	for {
+		outputSize, err := r.poll()
+		totalout += outputSize
+		//out.Write(d.outbuf[:outputSize])
+		l("Poll: %v %v %v %v\n", err, outputSize, len(in), r.inputSize)
+		if err == errOutputBufferFull {
+			l("Output buffer full, returning...")
+			return totalout, errOutputBufferFull
+		}
+		if r.finish() {
+			return totalout, nil
+		}
+	}
+	return totalout, errors.New("Ran out of input before finishing")
 }
 
 func logDecodeState(s decodeState) string {
@@ -125,68 +113,63 @@ func logDecodeState(s decodeState) string {
 	return fmt.Sprintf("%d", s)
 }
 
-func (d *decoder) poll(out []byte) (error, int, bool) {
+func (r *Reader) poll() (int, error) {
 
-	oi := &outputInfo{
-		buf:        out,
-		bufSize:    cap(out),
-		outputSize: 0,
-	}
+	r.outputIndex = 0
 
 	for {
-		l("-- poll, state is %v, input_size %d\n", logDecodeState(d.state), d.inputSize)
-		state := d.state
+		l("-- poll, state is %v, input_size %d\n", logDecodeState(r.state), r.inputSize)
+		state := r.state
 		switch state {
 		case decodeStateTagBit:
-			d.state = d.stateTagBit()
+			r.state = r.stateTagBit()
 		case decodeStateYieldLiteral:
-			d.state = d.stateYieldLiteral(oi)
+			r.state = r.stateYieldLiteral()
 		case decodeStateBackRefIndexMSB:
-			d.state = d.stateBackRefIndexMSB()
+			r.state = r.stateBackRefIndexMSB()
 		case decodeStateBackRefIndexLSB:
-			d.state = d.stateBackRefIndexLSB()
+			r.state = r.stateBackRefIndexLSB()
 		case decodeStateBackRefCountMSB:
-			d.state = d.stateBackRefCountMSB()
+			r.state = r.stateBackRefCountMSB()
 		case decodeStateBackRefCountLSB:
-			d.state = d.stateBackRefCountLSB()
+			r.state = r.stateBackRefCountLSB()
 		case decodeStateYieldBackRef:
-			d.state = d.stateYieldBackRef(oi)
+			r.state = r.stateYieldBackRef()
 		default:
 			log.Fatal("Unknown state: %v", state)
 		}
-		if d.state == state {
-			if oi.outputSize == cap(out) {
-				return nil, oi.outputSize, true
+		if r.state == state {
+			if r.outputIndex == cap(r.outputBuffer) {
+				return r.outputIndex, errOutputBufferFull
 			}
-			return nil, oi.outputSize, false
+			return r.outputIndex, nil
 		}
 	}
-
 }
 
-func (d *decoder) stateTagBit() decodeState {
-	bits := d.getBits(1)
-	if bits == NOBITS {
+func (r *Reader) stateTagBit() decodeState {
+	bits := r.getBits(1)
+	if bits == nobits {
 		return decodeStateTagBit
 	}
 	if bits > 0 {
 		return decodeStateYieldLiteral
 	}
-	if d.window > 8 {
+	if r.window > 8 {
 		return decodeStateBackRefIndexMSB
 	}
-	d.outputIndex = 0
+	r.outputBackRefIndex = 0
 	return decodeStateBackRefIndexLSB
 }
 
-func (d *decoder) stateYieldLiteral(oi *outputInfo) decodeState {
-	if oi.outputSize < oi.bufSize {
-		bits := d.getBits(8)
-		if bits == NOBITS {
+func (r *Reader) stateYieldLiteral() decodeState {
+	if r.outputIndex < r.outputSize {
+		bits := r.getBits(8)
+		if bits == nobits {
 			return decodeStateYieldLiteral
 		}
-		var mask uint16 = (1 << d.window) - 1
-		var c byte = byte(bits & 0xFF)
+		var mask uint16 = (1 << r.window) - 1
+		c := byte(bits & 0xFF)
 		l("-- mask 0x%02x\n", mask)
 
 		if isPrint(c) {
@@ -194,139 +177,139 @@ func (d *decoder) stateYieldLiteral(oi *outputInfo) decodeState {
 		} else {
 			l("-- emitting literal byte 0x%02x ('.')\n", c)
 		}
-		l("-- hsd->head_index 0x%02x\n", d.headIndex)
-		d.windowBuffer[uint16(d.headIndex)&mask] = c
-		d.headIndex++
-		d.push(oi, c)
+		l("-- hsd->head_index 0x%02x\n", r.headIndex)
+		r.windowBuffer[uint16(r.headIndex)&mask] = c
+		r.headIndex++
+		r.push(c)
 		return decodeStateTagBit
 	}
 	return decodeStateYieldLiteral
 }
 
-func (d *decoder) stateBackRefIndexMSB() decodeState {
-	bitCount := d.window
-	bits := d.getBits(bitCount - 8)
+func (r *Reader) stateBackRefIndexMSB() decodeState {
+	bitCount := r.window
+	bits := r.getBits(bitCount-8)
 	l("-- backref index (msb), got 0x%04x (+1)\n", bits)
-	if bits == NOBITS {
+	if bits == nobits {
 		return decodeStateBackRefIndexMSB
 	}
-	d.outputIndex = int(bits) << 8
+	r.outputBackRefIndex = int(bits) << 8
 	return decodeStateBackRefIndexLSB
 }
 
-func (d *decoder) stateBackRefIndexLSB() decodeState {
-	bitCount := d.window
+func (r *Reader) stateBackRefIndexLSB() decodeState {
+	bitCount := r.window
 	var bits uint16
 	if bitCount < 8 {
-		bits = d.getBits(bitCount)
+		bits = r.getBits(bitCount)
 	} else {
-		bits = d.getBits(8)
+		bits = r.getBits(8)
 	}
 	l("-- backref index (lsb), got 0x%04x (+1)\n", bits)
-	if bits == NOBITS {
+	if bits == nobits {
 		return decodeStateBackRefIndexLSB
 	}
-	d.outputIndex |= int(bits)
-	d.outputIndex++
-	backRefBitCount := d.lookahead
-	d.outputCount = 0
+	r.outputBackRefIndex |= int(bits)
+	r.outputBackRefIndex++
+	backRefBitCount := r.lookahead
+	r.outputCount = 0
 	if backRefBitCount > 8 {
 		return decodeStateBackRefCountMSB
 	}
 	return decodeStateBackRefCountLSB
 }
 
-func (d *decoder) stateBackRefCountMSB() decodeState {
-	backRefBitCount := d.lookahead
-	bits := d.getBits(backRefBitCount - 8)
+func (r *Reader) stateBackRefCountMSB() decodeState {
+	backRefBitCount := r.lookahead
+	bits := r.getBits(backRefBitCount-8)
 	l("-- backref count (msb), got 0x%04x (+1)\n", bits)
-	if bits == NOBITS {
+	if bits == nobits {
 		return decodeStateBackRefCountMSB
 	}
-	d.outputCount = int(bits) << 8
+	r.outputCount = int(bits) << 8
 	return decodeStateBackRefCountLSB
 }
 
-func (d *decoder) stateBackRefCountLSB() decodeState {
-	backRefBitCount := d.lookahead
+func (r *Reader) stateBackRefCountLSB() decodeState {
+	backRefBitCount := r.lookahead
 	var bits uint16
 	if backRefBitCount < 8 {
-		bits = d.getBits(backRefBitCount)
+		bits = r.getBits(backRefBitCount)
 	} else {
-		bits = d.getBits(8)
+		bits = r.getBits(8)
 	}
 	l("-- backref count (lsb), got 0x%04x (+1)\n", bits)
-	if bits == NOBITS {
+	if bits == nobits {
 		return decodeStateBackRefCountLSB
 	}
-	d.outputCount |= int(bits)
-	d.outputCount++
+	r.outputCount |= int(bits)
+	r.outputCount++
 	return decodeStateYieldBackRef
 }
 
-func (d *decoder) stateYieldBackRef(oi *outputInfo) decodeState {
-	count := oi.bufSize - oi.outputSize
+func (r *Reader) stateYieldBackRef() decodeState {
+	count := r.outputSize - r.outputIndex
 	if count > 0 {
-		if d.outputCount < count {
-			count = d.outputCount
+		if r.outputCount < count {
+			count = r.outputCount
 		}
 
-		var mask int = (1 << d.window) - 1
-		var negOffset int = d.outputIndex
+		mask := (1 << r.window) - 1
+		negOffset := r.outputBackRefIndex
 		l("-- emitting %d bytes from -%d bytes back\n", count, negOffset)
 		for i := 0; i < count; i++ {
-			c := d.windowBuffer[(d.headIndex-negOffset)&mask]
-			d.push(oi, c)
-			d.windowBuffer[d.headIndex&mask] = c
-			d.headIndex++
+			c := r.windowBuffer[(r.headIndex-negOffset)&mask]
+			r.push(c)
+			r.windowBuffer[r.headIndex&mask] = c
+			r.headIndex++
 			l("  -- ++ 0x%02x\n", c)
 		}
-		d.outputCount -= count
-		if d.outputCount == 0 {
+		r.outputCount -= count
+		if r.outputCount == 0 {
 			return decodeStateTagBit
 		}
 	}
 	return decodeStateYieldBackRef
 }
 
-func (d *decoder) getBits(count uint8) uint16 {
-	var accumulator uint16 = 0
+func (r *Reader) getBits(count uint8) uint16 {
+	var accumulator uint16
 
 	if count > 15 {
-		return NOBITS
+		return nobits
 	}
 	l("-- popping %d bit(s)\n", count)
-	if d.inputSize == 0 {
-		if d.bitIndex < (1 << (count - 1)) {
-			return NOBITS
+	if r.inputSize == 0 {
+		if r.bitIndex < (1 << (count - 1)) {
+			return nobits
 		}
 	}
 	var i uint8
 	for i = 0; i < count; i++ {
-		if d.bitIndex == 0x0 {
-			if d.inputSize == 0 {
+		if r.bitIndex == 0x0 {
+			if r.inputSize == 0 {
 				l("out of bits, suspending w/ accumulator of %u (0x%02x)\n", accumulator, accumulator)
-				return NOBITS
+				return nobits
 			}
-			l("buffer: %v", hex.EncodeToString(d.buffer))
-			l("buffer: %v", hex.EncodeToString(d.windowBuffer))
-			d.current = d.buffer[d.inputIndex]
-			l("  -- pulled byte 0x%02x at %d\n", d.current, d.inputIndex)
-			d.inputIndex++
-			if d.inputIndex == d.inputSize {
-				d.inputIndex = 0
-				d.inputSize = 0
+			l("buffer: %v", hex.EncodeToString(r.buffer))
+			l("buffer: %v", hex.EncodeToString(r.windowBuffer))
+			r.current = r.buffer[r.inputIndex]
+			l("  -- pulled byte 0x%02x at %d\n", r.current, r.inputIndex)
+			r.inputIndex++
+			if r.inputIndex == r.inputSize {
+				r.inputIndex = 0
+				r.inputSize = 0
 			}
-			d.bitIndex = 0x80
+			r.bitIndex = 0x80
 		}
 		accumulator <<= 1
-		if d.current&d.bitIndex > 0 {
+		if r.current& r.bitIndex > 0 {
 			accumulator |= 0x01
-			l("  -- got 1, accumulator 0x%04x, bit_index 0x%02x\n", accumulator, d.bitIndex)
+			l("  -- got 1, accumulator 0x%04x, bit_index 0x%02x\n", accumulator, r.bitIndex)
 		} else {
-			l("  -- got 0, accumulator 0x%04x, bit_index 0x%02x\n", accumulator, d.bitIndex)
+			l("  -- got 0, accumulator 0x%04x, bit_index 0x%02x\n", accumulator, r.bitIndex)
 		}
-		d.bitIndex >>= 1
+		r.bitIndex >>= 1
 	}
 	if count > 1 {
 		l("  -- accumulated %08x\n", accumulator)
@@ -334,27 +317,15 @@ func (d *decoder) getBits(count uint8) uint16 {
 	return accumulator
 }
 
-func (d *decoder) finish() bool {
-	switch d.state {
-	case decodeStateTagBit:
-		if d.inputSize == 0 {
+func (r *Reader) finish() bool {
+	switch r.state {
+	case decodeStateTagBit, decodeStateBackRefIndexLSB, decodeStateBackRefIndexMSB, decodeStateBackRefCountLSB, decodeStateBackRefCountMSB, decodeStateYieldLiteral:
+		if r.inputSize == 0 {
 			return true
 		}
 		return false
-	case decodeStateBackRefIndexLSB:
-	case decodeStateBackRefIndexMSB:
-	case decodeStateBackRefCountLSB:
-	case decodeStateBackRefCountMSB:
-		if d.inputSize == 0 {
-			return true
-		}
+	case decodeStateYieldBackRef:
 		return false
-	case decodeStateYieldLiteral:
-		if d.inputSize == 0 {
-			return true
-		}
-		return false
-
 	}
 	return false
 }
@@ -363,14 +334,14 @@ func isPrint(b byte) bool {
 	return b > 0x1f && b != 0x7f
 }
 
-func (d *decoder) push(oi *outputInfo, b byte) {
+func (r *Reader) push(b byte) {
 	if isPrint(b) {
 		l(" -- pushing byte: 0x%02x ('%c')\n", b, b)
 	} else {
 		l(" -- pushing byte: 0x%02x ('.')\n", b)
 	}
-	oi.buf[oi.outputSize] = b
-	oi.outputSize++
+	r.outputBuffer[r.outputIndex] = b
+	r.outputIndex++
 }
 
 func l(format string, v ...interface{}) {
--- heatshrink_test.go
+++ heatshrink_test.go
@@ -3,25 +3,31 @@ package goheatshrink
 import (
 	"testing"
 	"crypto/rand"
+	"log"
+	"bytes"
+	"io/ioutil"
 )
 
 
 func Test(t *testing.T) {
-	testdata := make([]byte, 1 << 16)
+	testdata := make([]byte, 3760)
 	rand.Read(testdata)
 	e := NewEncoder(8, 4)
+	log.Printf("Encoding...")
 	encoded, err := e.Encode(testdata)
 	if err != nil {
 		t.Errorf("Error encoding: %v", err)
 	}
-	d := NewDecoder(8, 4)
-	decoded, err := d.Decode(encoded)
+	r := bytes.NewBuffer(encoded)
+	d := NewReader(r, 8, 4)
+	log.Printf("Decoding...")
+	decoded, err := ioutil.ReadAll(d)
 	if err != nil {
 		t.Errorf("Error decoding: %v", err)
 	}
 	for i := range decoded {
 		if testdata[i] != decoded[i] {
-			t.Errorf("Different at: ", i, " data -> ", testdata[i], " decompressed -> ", decoded[i])
+			t.Errorf("Different at: %d data -> %v decompressed -> %v", i, testdata[i], decoded[i])
 			break
 		}
 	}
